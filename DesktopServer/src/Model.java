9d6c8878f5a40652cbe6d8bc7a3e9800fd932fd7
EricJimenez <56539020+EricJimenezRoldan@users.noreply.github.com>
2022-11-18 17:48:22 +0100

    SPEC 5
    Spec 5 completed

--- DesktopServer/src/Model.java
+++ DesktopServer/src/Model.java
@@ -51,7 +51,7 @@ public class Model {
     }
 
     
-    public boolean readXML(){
+    public boolean readXML() throws IllegalArgumentException{
 
         if(currentFile != null){
             
@@ -67,66 +67,147 @@ public class Model {
                 for(int i = 0; i < nodelistControls.getLength(); i++){
 
                     Element elementControls = (Element) nodelistControls.item(i);
-                    
-                    Border border = BorderFactory.createTitledBorder(elementControls.getAttribute("name"));
-                    CustomControlPanel controlPanel = new CustomControlPanel(elementControls.getAttribute("name"));
-                    controlPanel.setBorder(border);
-                    controlBlockData.add(controlPanel);
+                    if (elementControls.getAttribute("name").isEmpty()){
+                        JFrame jFrame = new JFrame();
+                        JOptionPane.showMessageDialog(jFrame, "The name tag is not configured correctly!!!");
+                        return false;
+                    }
+                    else{
+                        Border border = BorderFactory.createTitledBorder(elementControls.getAttribute("name"));
+                        CustomControlPanel controlPanel = new CustomControlPanel(elementControls.getAttribute("name"));
+                        controlPanel.setBorder(border);
+                        controlBlockData.add(controlPanel);
+                    }
                                 
                     //Bucle de switches
                     NodeList nodelistSwitch = elementControls.getElementsByTagName("switch");   
                     for(int j = 0; j < nodelistSwitch.getLength(); j++){
-                        Element elementSwitch = (Element)nodelistSwitch.item(j); //Current switch    
-
-                        CustomSwitch customSwitch = new CustomSwitch(elementSwitch.getAttribute("id"), elementControls.getAttribute("name"), elementSwitch.getTextContent(), elementSwitch.getAttribute("default"));
-                        customComponents.add(customSwitch);
+                        Element elementSwitch = (Element)nodelistSwitch.item(j); //Current switch   
+                        try {
+                            Integer.parseInt(elementSwitch.getAttribute("id"));
+                            if (elementSwitch.getAttribute("default").equals("on") || elementSwitch.getAttribute("default").equals("off")){
+                                CustomSwitch customSwitch = new CustomSwitch(elementSwitch.getAttribute("id"), elementControls.getAttribute("name"), elementSwitch.getTextContent(), elementSwitch.getAttribute("default"));
+                                customComponents.add(customSwitch);
+                            }
+                            else{
+                                JFrame jFrame = new JFrame();
+                                JOptionPane.showMessageDialog(jFrame, "The default option is not correct: Switch default option!!!");
+                                return false;
+                            }
+                        } catch (NumberFormatException e) {
+                            JFrame jFrame = new JFrame();
+                            JOptionPane.showMessageDialog(jFrame, "The ID must be a number: Switch ID!!!\n"+e.getMessage());
+                            return false;
+                        } 
                     }
                     
                     //Bucle de slider
                     NodeList nodelistSlider = elementControls.getElementsByTagName("slider");     
                     for(int k = 0; k < nodelistSlider.getLength(); k++){
                         Element elementSlider = (Element)nodelistSlider.item(k); //Current switch    
-                        
-                        CustomSlider customSlider = new CustomSlider(elementSlider.getAttribute("id"), elementControls.getAttribute("name"), elementSlider.getTextContent(),Float.parseFloat(elementSlider.getAttribute("default")), Float.parseFloat(elementSlider.getAttribute("min")), Float.parseFloat(elementSlider.getAttribute("max")), Float.parseFloat(elementSlider.getAttribute("step")));
+                        try {
+                            Integer.parseInt(elementSlider.getAttribute("id"));
+                            if (Integer.parseInt(elementSlider.getAttribute("default")) > Float.parseFloat(elementSlider.getAttribute("max")) || Integer.parseInt(elementSlider.getAttribute("default")) < Float.parseFloat(elementSlider.getAttribute("min"))){
+                                JFrame jFrame = new JFrame();
+                                JOptionPane.showMessageDialog(jFrame, "The default number is not correct: Slider default number!!!");
+                                return false;
+                            }
+                            else {
+                                if (Float.parseFloat(elementSlider.getAttribute("step")) > Float.parseFloat(elementSlider.getAttribute("max")) || Float.parseFloat(elementSlider.getAttribute("step")) < Float.parseFloat(elementSlider.getAttribute("min"))){
+                                    JFrame jFrame = new JFrame();
+                                    JOptionPane.showMessageDialog(jFrame, "The step number is not correct: Slider step number!!!");
+                                    return false;
+                                }
+                                else {
+                                    CustomSlider customSlider = new CustomSlider(elementSlider.getAttribute("id"), elementControls.getAttribute("name"), elementSlider.getTextContent(),Float.parseFloat(elementSlider.getAttribute("default")), Float.parseFloat(elementSlider.getAttribute("min")), Float.parseFloat(elementSlider.getAttribute("max")), Float.parseFloat(elementSlider.getAttribute("step")));
 
-                        customComponents.add(customSlider);
+                                    customComponents.add(customSlider);
+                                }
+                            }
+                        } catch (NumberFormatException e) {
+                            JFrame jFrame = new JFrame();
+                            JOptionPane.showMessageDialog(jFrame, "The ID must be a number: Slider ID!!!\n"+e.getMessage());
+                            return false;
+                        }
+                        
                     }
                     
                     //Bucle sensor
                     NodeList nodelistSensor = elementControls.getElementsByTagName("sensor");
                     for(int l = 0; l < nodelistSensor.getLength(); l++){
-                        Element elementSensor = (Element)nodelistSensor.item(l); //Current switch  
-                    
-                        CustomSensor customSensor = new CustomSensor(elementSensor.getAttribute("id"), elementControls.getAttribute("name"), elementSensor.getTextContent(), elementSensor.getAttribute("units"), elementSensor.getAttribute("thresholdlow"), elementSensor.getAttribute("thresholdhigh"));
+                        Element elementSensor = (Element)nodelistSensor.item(l); //Current switch
+                        try {
+                            Integer.parseInt(elementSensor.getAttribute("id"));
+                        } catch (NumberFormatException e) {
+                            JFrame jFrame = new JFrame();
+                            JOptionPane.showMessageDialog(jFrame, "The ID must be a number: Sensor ID!!!\n"+e.getMessage());
+                            return false;
+                        }  
+                        if (Integer.parseInt(elementSensor.getAttribute("thresholdlow")) < Integer.parseInt(elementSensor.getAttribute("thresholdhigh"))){
+                            if (elementSensor.getAttribute("units").equals("ºC") || elementSensor.getAttribute("units").equals("ºF") || elementSensor.getAttribute("units").equals("K")){
+                                CustomSensor customSensor = new CustomSensor(elementSensor.getAttribute("id"), elementControls.getAttribute("name"), elementSensor.getTextContent(), elementSensor.getAttribute("units"), elementSensor.getAttribute("thresholdlow"), elementSensor.getAttribute("thresholdhigh"));
                         
-                        customComponents.add(customSensor);
+                                customComponents.add(customSensor);
+                            }
+                            else{
+                                JFrame jFrame = new JFrame();
+                                JOptionPane.showMessageDialog(jFrame, "Allowable temperature units are Kelvin(K), Celsius(ºC) and Fahrenheit(ºF)!!!");
+                                return false;
+                            }
+                        }
+                        else {
+                            JFrame jFrame = new JFrame();
+                            JOptionPane.showMessageDialog(jFrame, "Thresholdlow is bigger than thresholdhigh!!!");
+                            return false;
+                        }
+                        
                     
                     }
                     
                     //Bucle dropdown
-                    NodeList nodelistDropdown = elementControls.getElementsByTagName("dropdown");
-                    for(int m = 0; m < nodelistDropdown.getLength(); m++){
-                        ArrayList<String> optionsArrayList =  new ArrayList<String>();
-                        Element elementDropdown = (Element)nodelistDropdown.item(m);
-                        //Bucle options
-                        NodeList nodelistOptions = elementDropdown.getElementsByTagName("option");
-                        for(int n = 0; n < nodelistOptions.getLength(); n++){
-                            Element elementOption = (Element)nodelistOptions.item(n);
-                            optionsArrayList.add(elementOption.getTextContent());
-                        }
-                    
-                        CustomDropdown customDropdown = new CustomDropdown(elementDropdown.getAttribute("id"), elementControls.getAttribute("name"), elementDropdown.getTextContent(), Integer.parseInt(elementDropdown.getAttribute("default")), optionsArrayList);
-                        
-                        customComponents.add(customDropdown);
-                    }
-                }
+                    try {
+                        NodeList nodelistDropdown = elementControls.getElementsByTagName("dropdown");
+                        for(int m = 0; m < nodelistDropdown.getLength(); m++){
+                            ArrayList<String> optionsArrayList =  new ArrayList<String>();
+                            Element elementDropdown = (Element)nodelistDropdown.item(m);
+                            try {
+                                Integer.parseInt(elementDropdown.getAttribute("id"));
+                            } catch (NumberFormatException e) {
+                                JFrame jFrame = new JFrame();
+                                JOptionPane.showMessageDialog(jFrame, "The ID must be a number: Dropdown ID!!!\n"+e.getMessage());
+                                return false;
+                            }
+                            //Bucle options
+                            NodeList nodelistOptions = elementDropdown.getElementsByTagName("option");
+                            for(int n = 0; n < nodelistOptions.getLength(); n++){
+                                Element elementOption = (Element)nodelistOptions.item(n);
+                                optionsArrayList.add(elementOption.getTextContent());
+                            }
 
+                            if (Integer.parseInt(elementDropdown.getAttribute("default")) < nodelistOptions.getLength()) {
+                                CustomDropdown customDropdown = new CustomDropdown(elementDropdown.getAttribute("id"), elementControls.getAttribute("name"), elementDropdown.getTextContent(), Integer.parseInt(elementDropdown.getAttribute("default")), optionsArrayList);
+                            
+                                customComponents.add(customDropdown);
+                            }
+                            else{
+                                JFrame jFrame = new JFrame();
+                                JOptionPane.showMessageDialog(jFrame, "The default number is not correct: Dropdown default number");
+                                return false;
+                            }
+                            
+                        }
+                    } catch (IllegalArgumentException e){
+                        JFrame jFrame = new JFrame();
+                        JOptionPane.showMessageDialog(jFrame, "Be careful with the characters you put!\n"+e.getMessage());
+                        return false;
+                    } 
+                }  
             } catch (ParserConfigurationException e) {
                 e.printStackTrace();
                 return false;
             } catch (SAXException e) {
             	JFrame jFrame = new JFrame();
-                JOptionPane.showMessageDialog(jFrame, "Remember to put the labels right!\n"+e.getMessage());
+                JOptionPane.showMessageDialog(jFrame, "Remember to put the labels right! It may also be that you have not selected an XML file\n"+e.getMessage());
                 return false;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -135,7 +216,7 @@ public class Model {
             	JFrame jFrame = new JFrame();
                 JOptionPane.showMessageDialog(jFrame, "Aware of! Don't put letters and special characters where they don't belong, also be careful with the titles of the variables.\n"+e.getMessage());
                 return false;
-            }
+            } 
          
         }
         else{
